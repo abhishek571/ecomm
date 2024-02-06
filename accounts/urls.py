c63cd29465223af708a5639e1893f92dc9da78d0
ginchandesu <142890093+ginchandesu@users.noreply.github.com>
2024-02-06 14:25:21 +0530

    Initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from accounts.views import login_page,register_page,activate_email , cart , add_to_cart
+# from products.views import 
+urlpatterns = [
+    path('login/' , login_page , name="login" ),
+    path('register/' , register_page , name="register"),
+    path('activate/<email_token>/' , activate_email , name="activate_email"),
+    path('cart/' , cart , name="cart"),
+    path('add-to-cart/<uid>/', add_to_cart , name="add_to_cart")
+]
